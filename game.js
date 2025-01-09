f840691b72368884f42e4b76a050447e35af224b
Artyom Ulanchik <artyom.u@outlook.com>
2025-01-09 19:16:56 +0000

    first test - start game

--- game.js
+++ game.js
@@ -1,3 +1,23 @@
-let TicTacToe;
+let TicTacToe = class {
+    constructor() {
+        this.board = Array(9).fill(null);
+        this.gameStatus = 'ongoing';
+        this.currentPlayer = 'X';
+    }
 
-module.exports = TicTacToe;
+    startGame() {
+        this.board = Array(9).fill(null);
+        this.gameStatus = 'ongoing';
+        this.currentPlayer = 'X';
+    }
+
+    getBoard() {
+        return this.board;
+    }
+
+    getGameStatus() {
+        return this.gameStatus;
+    }
+};
+
+module.exports.default = TicTacToe;
--- game.test.js
+++ game.test.js
@@ -1,5 +1,16 @@
-let TicTacToe = require('./game.js');
+let TicTacToe = require('./game.js').default;
 
-describe('The tic tac toe game', () => { 
+describe('The tic tac toe game', () => {
+    let game;
+
+    beforeEach(() => {
+        game = new TicTacToe();
+    });
+
+    it('should create empty board and initialize new game', () =>  {
+        game.startGame();
+        expect(game.getGameStatus()).toBe('ongoing');
+        expect(game.getBoard()).toEqual([null, null, null, null, null, null, null, null, null]);
+    });
 
 });
