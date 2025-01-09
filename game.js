c0e11b16ea1c827ae268d7bf5a88ef11d538fc65
Artyom Ulanchik <artyom.u@outlook.com>
2025-01-09 21:06:18 +0000

    draw and winner detection + refactor

--- game.js
+++ game.js
@@ -1,8 +1,6 @@
 let TicTacToe = class {
     constructor() {
-        this.board = Array(9).fill(null);
-        this.gameStatus = 'ongoing';
-        this.currentPlayer = 'X';
+        this.startGame();
     }
 
     startGame() {
@@ -20,12 +18,38 @@ let TicTacToe = class {
     }
 
     makeMove(position) {
-       if (this.board[position] === null && this.gameStatus === 'ongoing') {
-            this.board[position] = this.currentPlayer;
-            this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
-        } else {
+        if (this.board[position] !== null || this.gameStatus !== 'ongoing') {
             throw new Error('Invalid move');
         }
+
+        this.board[position] = this.currentPlayer;
+
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        const hasWinner = winningCombinations.some(([a, b, c]) =>
+            this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]
+        );
+
+        if (hasWinner) {
+            this.gameStatus = `winner:${this.currentPlayer}`;
+            return;
+        }
+
+        if (this.board.every(cell => cell !== null)) {
+            this.gameStatus = 'draw';
+            return;
+        }
+
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
     }
 };
 
